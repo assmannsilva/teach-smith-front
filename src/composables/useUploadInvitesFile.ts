c0e1fc0566a1c6ad344cf7a50e2e7513cad495d9
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-07-22 20:00:10 -0300

    feat:  composable upload invite

---
+++ src/composables/useUploadInvitesFile.ts
@@ -0,0 +1,31 @@
+import studentService from "@/services/student.service";
+import type { BulkInviteResponse, BulkInviteStatus } from "@/types/invites";
+import { ref } from "vue";
+
+export function useUploadInvitesFile() {
+  const importFile = ref<File | null>(null);
+
+  const status = ref<BulkInviteStatus | null>(null);
+  const dispatchedCount = ref< number| null >(0);
+  const errors = ref<string[] | null>(null);
+
+  async function setResponseToProperties(response: BulkInviteResponse) {
+    status.value = response.status;
+    dispatchedCount.value = response.dispatched
+    errors.value = response.errors
+  }
+
+  async function inviteMultipleStudents() {
+    if(!importFile.value) return;
+    const response = await studentService.bulkInvite(importFile.value)
+    setResponseToProperties(response)
+  }
+
+  return {
+    importFile,
+    status,
+    dispatchedCount,
+    errors,
+    inviteMultipleStudents
+  };
+}
